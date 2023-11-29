9ba05e07e97b7c5f411327893a402fd93f6fc9d9
KumaranS <118594761+MrKumaran@users.noreply.github.com>
2023-11-29 21:07:25 +0530

    Added comments and formatted code a bit

--- subStringFInder/main.py
+++ subStringFInder/main.py
@@ -1,9 +1,11 @@
-string = input()
-startvalue = 0
-substring = []
+string = input()  # Getting input
+startvalue = 0  # set Start value to Zero
+substring = []  # List that holds substring of main string
 length = len(string)
-for i in range(0, length):
-    for endvalue in range(i+1, length+1):
-        substring.append(string[startvalue:endvalue])
+for i in range(length):
+    for endvalue in range(i + 1, length + 1):
+        substring.append(string[startvalue:endvalue])  # Appending sliced string to substring list
     startvalue += 1
+
 print(substring)
+print(int(length * (length + 1) / 2))  # Maximum number substring possible
