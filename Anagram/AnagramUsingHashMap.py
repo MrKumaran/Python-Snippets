d54c0240c9d3fa5adbee35680f93608a7495ee72
KumaranS <mrkumarancse@gmail.com>
2023-12-11 23:49:40 +0530

    Commented

--- Anagram/AnagramUsingHashMap.py
+++ Anagram/AnagramUsingHashMap.py
@@ -1,18 +1,23 @@
 def isanagram(string1, string2):
-    hashmap = {}
+    hashmap = {}  # Empty directory
 
     for character in string1:
-        if character in hashmap:
-            hashmap[character] += 1
-        else:
+        if character not in hashmap:
+            # add {character : value}
             hashmap[character] = 1
+        else:
+            # if character already in directory increment value by 1
+            hashmap[character] += 1
 
     for character in string2:
         if character not in hashmap:
+            # if character not in directory return False
             return False
         elif hashmap[character] == 1:
+            # if character value is 1 del character
             del hashmap[character]
         else:
+            # if character value is more than decrement value by 1
             hashmap[character] -= 1
 
     return not hashmap
@@ -20,4 +25,5 @@ def isanagram(string1, string2):
 if __name__ == '__main__':
     wordOne = input("Enter the first string: ")
     wordTwo = input("Enter the second string: ")
+    # printing result
     print(isanagram(wordOne.lower(), wordTwo.lower()))
--- Anagram/AnagramUsingSortMethod.py
+++ Anagram/AnagramUsingSortMethod.py
@@ -1,10 +1,13 @@
-def isanagram(str1, str2):
+def isanagram(str1, str2):  # check for anagram or not
     if len(str1) != len(str2):
+        # if length of strings are not same then automatically it is not anagram
         return False
     else:
+        # sorting the string and comparing two strings
         return sorted(str1) == sorted(str2)
 
 if __name__ == '__main__':
     word1 = input("Enter the first string: ")
     word2 = input("Enter the second string: ")
+    # printing the result
     print(isanagram(word1.lower(), word2.lower()))
