8fd9c7133337e2ab278aebd1d3be6f2bc01216cb
KumaranS <mrkumarancse@gmail.com>
2023-12-14 00:14:48 +0530

    initial commit

--- GUI_in_Python/GUI_For_Calculator.py
+++ GUI_in_Python/GUI_For_Calculator.py
@@ -22,48 +22,51 @@ text.pack(padx=20, pady=30)
 
 # frame for buttons in grid to place like numberpads
 buttonFrame = tk.Frame(root)
+# declaring number of column in numberpad
 buttonFrame.columnconfigure(0, weight=1)
 buttonFrame.columnconfigure(1, weight=1)
 buttonFrame.columnconfigure(2, weight=1)
 
-btn1 = tk.Button(buttonFrame, text="1", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+# setting buttons properties
+btn1 = tk.Button(buttonFrame, text="1", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn1.grid(row=0, column=0)
+# buttons need to show as grid in root
+btn1.grid(row=0, column=0, sticky=tk.W + tk.E)
 
-btn2 = tk.Button(buttonFrame, text="2", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn2 = tk.Button(buttonFrame, text="2", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn2.grid(row=0, column=1)
+btn2.grid(row=0, column=1, sticky=tk.W + tk.E)
 
-btn3 = tk.Button(buttonFrame, text="3", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn3 = tk.Button(buttonFrame, text="3", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn3.grid(row=0, column=2)
+btn3.grid(row=0, column=2, sticky=tk.W + tk.E)
 
-btn4 = tk.Button(buttonFrame, text="4", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn4 = tk.Button(buttonFrame, text="4", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn4.grid(row=1, column=0)
+btn4.grid(row=1, column=0, sticky=tk.W + tk.E)
 
-btn5 = tk.Button(buttonFrame, text="5", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn5 = tk.Button(buttonFrame, text="5", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn5.grid(row=1, column=1)
+btn5.grid(row=1, column=1, sticky=tk.W + tk.E)
 
-btn6 = tk.Button(buttonFrame, text="6", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn6 = tk.Button(buttonFrame, text="6", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn6.grid(row=1, column=2)
+btn6.grid(row=1, column=2, sticky=tk.W + tk.E)
 
-btn7 = tk.Button(buttonFrame, text="7", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn7 = tk.Button(buttonFrame, text="7", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn7.grid(row=2, column=0)
+btn7.grid(row=2, column=0, sticky=tk.W + tk.E)
 
-btn8 = tk.Button(buttonFrame, text="8", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn8 = tk.Button(buttonFrame, text="8", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn8.grid(row=2, column=1)
+btn8.grid(row=2, column=1, sticky=tk.W + tk.E)
 
-btn9 = tk.Button(buttonFrame, text="9", width=10, height=1, borderwidth=15, foreground="Gold", background="Red",
+btn9 = tk.Button(buttonFrame, text="9", width=10, height=1, foreground="Gold", background="grey",
                  activebackground="Blue", activeforeground="Red", font=('Arial', 15, 'bold'))
-btn9.grid(row=2, column=2)
+btn9.grid(row=2, column=2, sticky=tk.W+tk.E)
 
+# button frame properties to stretch to fill in x-axis
 buttonFrame.pack(fill='x', pady=1)
-
 
 # Exit click button command = {Instructions when button pressed/clicked}
 # this EXIT button quit the GUI
