454138a7adcfb359ea7a432215587df39bf5cb1e
KumaranS <mrkumarancse@gmail.com>
2023-12-17 19:16:17 +0530

    Commented

---
+++ unMatchedStringIndexFinder/README.txt
@@ -0,0 +1,3 @@
+Gets two strings (or array of number if modifications made)
+
+return index at where two strings are different
--- unMatchedStringIndexFinder/unMatchStringIndexFinder.py
+++ unMatchedStringIndexFinder/unMatchStringIndexFinder.py
@@ -1,9 +1,12 @@
+# Getting Two string from user
 string1 = input('Enter String 1: ')
 string2 = input('Enter String 2: ')
-
+# zipping each character from string1 and string 2 together
 zip_string = zip(string1, string2)
+# enumerating zip_string list with index for each list
 enum_string = enumerate(zip_string)
-for i, (a,b) in enum_string:
+# taking each element in enum_string and evaluating it
+for i, (a, b) in enum_string:
+    # check not equal element and print it indexes
     if a != b:
         print(f'Unmatched Index: {i}')
-
