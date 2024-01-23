a546f64a87b8d44d08ed2f07e31b795a5a7e684a
kumarans <mrkumarancse@gmail.com>
2024-01-23 22:05:55 +0530

    Initial commit

--- generatingRandomNumbers/randomNumbersGenerators.py
+++ generatingRandomNumbers/randomNumbersGenerators.py
@@ -1,11 +1,11 @@
 import random
 import time
 
-
+# generates RandomNumbers b/w 1 to 1 billion
 def randomNumberGenerator():
     return random.randint(1,1000_000_000)
 
-
+# driver code
 if __name__ == '__main__':
     startTIme = time.time()
     limit = int(input("How many"))
