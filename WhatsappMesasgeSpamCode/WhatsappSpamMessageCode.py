7a008c0b9d6ab887daa1c6f004decbb260fd25a9
kumarans <mrkumarancse@gmail.com>
2024-03-18 21:09:41 +0530

    First Change.

--- WhatsappMesasgeSpamCode/WhatsappSpamMessageCode.py
+++ WhatsappMesasgeSpamCode/WhatsappSpamMessageCode.py
@@ -2,7 +2,7 @@ import random
 import pywhatkit
 import time
 
-phone_number = ""  # Enter Person phone number whom you wanna send message
+phone_number = ""  # Enter Person phone number whom you wanna send message with country code
 waitTime = 10  # How long do you want to wait before sending your message(in seconds min 15s)
 word = ("Hola", "Bello")  # add multiple words in word variable
 NumberOfTimes = 2  # Number of times message to be sent
