95b37be197aa6be0fe36a979d32f3ac54b84d5fd
kumarans <mrkumarancse@gmail.com>
2024-03-18 21:01:40 +0530

    First Change.

--- WhatsappMesasgeSpamCode/WhatsappSpamMessageCode.py
+++ WhatsappMesasgeSpamCode/WhatsappSpamMessageCode.py
@@ -4,7 +4,7 @@ import time
 
 phone_number = ""  # Enter Person phone number whom you wanna send message
 waitTime = 10  # How long do you want to wait before sending your message(in seconds min 15s)
-word = ('Hola', "Bello")  # add multiple words in word variable
+word = ("Hola", "Bello")  # add multiple words in word variable
 NumberOfTimes = 2  # Number of times message to be sent
 for ran in range(NumberOfTimes):
     pywhatkit.sendwhatmsg_instantly(f"{phone_number}", random.choice(word), wait_time=waitTime, tab_close=True)
