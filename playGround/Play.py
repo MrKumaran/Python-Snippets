ef5d7c1dfa6de762aafc59c96cc5e8e0ead04f1b
kumarans <mrkumarancse@gmail.com>
2024-07-06 23:31:17 +0530

    An Update!

--- playGround/Play.py
+++ playGround/Play.py
@@ -567,9 +567,10 @@ for c in range(len(calorie)):
     walk_distance = walk_distance + ((2**c)*m)
 
 print(walk_distance)
-
+"""
 
 # play_18
+"""
 def unboundedKnapsack(k, arr):
     dp = [0] * (k + 1)
     for i in range(1, k + 1):
@@ -584,3 +585,26 @@ n = sum([int(n[i]) for i in range(len(n))]) * k
 
 return superDigit(str(n), 1) if n > 9 else n
 """
+
+# Play 18 - Weekly coding
+"""
+n = 8
+k = 5
+contests = [[13, 1], [10, 1], [9, 1], [8, 1], [13, 1], [12, 1], [18, 1], [13, 1]]
+importantContest = []
+notImportantContest = []
+for i in contests:
+    if i[1] == 1:
+        importantContest.append(i[0])
+    else:
+        notImportantContest.append(i[0])
+luck = sum(notImportantContest)
+win = 0
+for _ in range(len(importantContest)-k):
+    win += min(importantContest)
+    importantContest.remove(min(importantContest))
+luck = luck + sum(importantContest) - win
+return luck
+"""
+
+
