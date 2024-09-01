7b8006bd059f396334c5feb1f8abeda773642dd2
Kumaran S <kumaran.s@velammal.edu.in>
2024-09-01 23:17:15 +0530

    eff fibonacci

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,35 @@
+import time as t
+
+
+def fib_eff(n):
+    fib =[0, 1]
+    if n == 0:
+        return fib
+    elif n == 1:
+        return fib
+    else:
+        for _ in range(2, n+1):
+            fib.append(fib[-1] + fib[-2])
+        return fib[-1]
+
+
+def Fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+
+n = int(input())
+start = t.time()
+fibi = fib_eff(n)
+end = t.time()
+print(fibi)
+print("Time taken: ", end-start)
+start = t.time()
+fibi = Fibonacci(n)
+end = t.time()
+print(fibi)
+print("Time taken: ", end-start)
